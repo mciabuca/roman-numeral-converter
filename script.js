d256c65ad8c58064b736172b2974a35a12740907
Michael Ciabuca <other404@pm.me>
2024-01-08 21:17:22 +0200

    created Roman Numeral Converter HTML, CSS, and JavaScript logic

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('convert-btn').addEventListener('click', function() {
+    var num = document.getElementById('number').value;
+    var output = document.getElementById('output');
+
+    if (num === '' || isNaN(num)) {
+        output.textContent = 'Please enter a valid number';
+    } else if (num < 1) {
+        output.textContent = 'Please enter a number greater than or equal to 1';
+    } else if (num >= 4000) {
+        output.textContent = 'Please enter a number less than or equal to 3999';
+    } else {
+        output.textContent = convertToRoman(num);
+    }
+});
+
+function convertToRoman(num) {
+    var roman = {
+        M: 1000, CM: 900, D: 500, CD: 400,
+        C: 100, XC: 90, L: 50, XL: 40,
+        X: 10, IX: 9, V: 5, IV: 4, I: 1
+    };
+    var str = '';
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
+}
